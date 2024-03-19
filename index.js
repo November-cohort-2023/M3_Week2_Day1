e25c072b78bde10204c65075b840f70d653570d0
Omar <omaraakamal@gmail.com>
2024-03-19 22:06:29 +0400

    Created Model

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ const mongoose = require('mongoose')
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/imdb")
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then(res => console.log(`Connected to Mongo! Database name: "${res.connections[0].name}"`))
   .catch(err => console.error("Error connecting to mongo", err));
+
+
+//   model() takes two arguments:
+//          1. Name of the Model
+//          2. Schema: outline of my object. How many key value pairs and the data types
+  const Movie = mongoose.model('Movie',{
+    title: String,
+    year:Number,
+    director:String,
+    duration:String,
+    genre:Array,
+    rate:Number
+
+  })
+
+  
+
+
+  app.listen(5005)
